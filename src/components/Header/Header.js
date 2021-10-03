6b758652ac7f7b76ed4ae37088f9a92c0319379d
coders-sohan <coders.sohan.0216@gmail.com>
2021-10-04 02:01:22 +0600

    header updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,12 +6,9 @@ const Header = () => {
 	return (
 		<header className=" bg-gray-100 border-b border-gray-300">
 			<div className="container mx-auto flex flex-wrap py-3 flex-col md:flex-row items-center">
-				<a
-					className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
-					href="/"
-				>
+				<NavLink to="/">
 					<img src={logo} alt="" />
-				</a>
+				</NavLink>
 				<nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
 					<NavLink
 						className="mr-5 py-2 px-4 text-base rounded-md font-semibold"
@@ -43,7 +40,10 @@ const Header = () => {
 					</NavLink>
 				</nav>
 				<button className="inline-flex items-center bg-indigo-500 text-white border-0 py-2 px-4 focus:ring-2 ring-indigo-700 hover:bg-indigo-700 hover:text-white rounded text-base mt-4 md:mt-0 transition duration-300 ease-linear">
-					<span className="mr-2">Login</span><span><i class="fas fa-sign-in-alt text-sm"></i></span>
+					<span className="mr-2">Login</span>
+					<span>
+						<i class="fas fa-sign-in-alt text-sm"></i>
+					</span>
 				</button>
 			</div>
 		</header>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is home page</h1>
+        <div className="container mx-auto">
+            <h1 className="text-5xl py-10">This is home page</h1>
         </div>
     );
 };
