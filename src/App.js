5035c2b99348016451f9197fb505049ea819c9e3
coders-sohan <coders.sohan.0216@gmail.com>
2021-10-04 01:53:40 +0600

    header updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Courses from "./components/Courses/Courses";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
 	return (
-		<div>
-			<Header></Header>
-			<Footer></Footer>
-		</div>
+		<>
+			<Router>
+				<Header></Header>
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/home">
+						<Home />
+					</Route>
+					<Route path="/about">
+						<About />
+					</Route>
+					<Route path="/courses">
+						<Courses />
+					</Route>
+					<Route path="/contact">
+						<Contact />
+					</Route>
+					<Route exact path="*">
+						<NotFound />
+					</Route>
+				</Switch>
+				<Footer></Footer>
+			</Router>
+		</>
 	);
 }
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,53 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../../img/educlub.png";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>This is header section</h1>
-        </div>
-    );
+	return (
+		<header className=" bg-gray-100 border-b border-gray-300">
+			<div className="container mx-auto flex flex-wrap py-3 flex-col md:flex-row items-center">
+				<a
+					className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+					href="/"
+				>
+					<img src={logo} alt="" />
+				</a>
+				<nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+					<NavLink
+						className="mr-5 py-2 px-4 text-base rounded-md font-semibold"
+						activeClassName="text-gray-900 bg-red-300"
+						to="/home"
+					>
+						Home
+					</NavLink>
+					<NavLink
+						className="mr-5 py-2 px-4 text-base rounded-md font-semibold"
+						activeClassName="text-gray-900 bg-red-300"
+						to="/about"
+					>
+						About
+					</NavLink>
+					<NavLink
+						className="mr-5 py-2 px-4 text-base rounded-md font-semibold"
+						activeClassName="text-gray-900 bg-red-300"
+						to="/courses"
+					>
+						Courses
+					</NavLink>
+					<NavLink
+						className="mr-5 py-2 px-4 text-base rounded-md font-semibold"
+						activeClassName="text-gray-900 bg-red-300"
+						to="/contact"
+					>
+						Contact
+					</NavLink>
+				</nav>
+				<button className="inline-flex items-center bg-indigo-500 text-white border-0 py-2 px-4 focus:ring-2 ring-indigo-700 hover:bg-indigo-700 hover:text-white rounded text-base mt-4 md:mt-0 transition duration-300 ease-linear">
+					<span className="mr-2">Login</span><span><i class="fas fa-sign-in-alt text-sm"></i></span>
+				</button>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
